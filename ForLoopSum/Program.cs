46616e8dbf80a6b03bfa941c28402b34f8ee3f72
cris <crisuke123@gmail.com>
2022-10-16 19:01:03 +0300

    add week code

---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,10 @@
+﻿
+int Sum = 0 ;  
+
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"i ={i}");
+    Console.WriteLine($"Current total:{Sum}");
+    Sum = Sum + i ;
+}
+Console.WriteLine($"Final total {Sum}");
